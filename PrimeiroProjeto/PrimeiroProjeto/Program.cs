2124ad1b033c1055ba37692d4ee5486b76f4218c
Junior <51051265+JuniorSantosSilva@users.noreply.github.com>
2023-11-27 12:23:21 -0300

    Add files via upload

---
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Diga olá para o mundo: ");
+
+            string a = Console.ReadLine();
+            Console.WriteLine(a);
+            
+
+        }
+    }
+}
